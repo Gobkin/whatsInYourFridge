9b69af8de8b388c86cfbc57ce07a04ea77b2634f
Roman <gobkins@gmail.com>
2019-09-09 12:36:27 -0400

    fixes

---
+++ src/firebaseConfig.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDnCAoRpLq__JRIdQava1AU5laNdKIzUeg",
+    authDomain: "whats-in-your-fridge-561e0.firebaseapp.com",
+    databaseURL: "https://whats-in-your-fridge-561e0.firebaseio.com",
+    projectId: "whats-in-your-fridge-561e0",
+    storageBucket: "whats-in-your-fridge-561e0.appspot.com",
+    messagingSenderId: "555250619075",
+    appId: "1:555250619075:web:5cfeb80a3750faf7628876"
+};
+// firebase.initializeApp(firebaseConfig);
+
+export default firebaseConfig;
